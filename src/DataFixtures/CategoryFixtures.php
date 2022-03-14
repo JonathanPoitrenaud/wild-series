403f121f8bdc3ec575c13be3a26ea47c1a09bbc5
grayfox <poitrenaudjonathan@gmail.com>
2022-03-14 21:43:10 +0100

    Quest 17 - Advance fixtures

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -8,13 +8,12 @@ use Doctrine\Persistence\ObjectManager;
 
 class CategoryFixtures extends Fixture
 {
-    const CATEGORIES = [
+    public const CATEGORIES = [
         'Action',
         'Aventure',
         'Animation',
         'Fantastique',
         'Horreur',
-        'Humour',
     ];
 
     public function load(ObjectManager $manager): void
@@ -22,8 +21,8 @@ class CategoryFixtures extends Fixture
         foreach (self::CATEGORIES as $key => $categoryName) {
             $category = new Category();
             $category->setName($categoryName);
-
             $manager->persist($category);
+            $this->addReference('category_' . $key, $category);
         }
         $manager->flush();
     }
---
+++ src/DataFixtures/ProgramFixtures.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Program;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class ProgramFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+        $program = new Program();
+        $program->setTitle('Walking dead');
+        $program->setSynopsis('Dead are not so dead !');
+        $program->setPoster('https://m.media-amazon.com/images/M/MV5BZmFlMTA0MmUtNWVmOC00ZmE1LWFmMDYtZTJhYjJhNGVjYTU5XkEyXkFqcGdeQXVyMTAzMDM4MjM0._V1_.jpg');
+        $program->setCategory($this->getReference('category_0'));
+
+        for ($i=0; $i < count(ActorFixtures::ACTORS); $i++) {
+            $program->addActor($this->getReference('actor_' . $i));
+        }
+        $manager->persist($program);
+        $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            ActorFixtures::class,
+            CategoryFixtures::class,
+        ];
+    }
+}
